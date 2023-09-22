62b269695db031090ab13e372406b9f4384d1828
NaN-Didital <nan-didital@192.168.50.53>
2023-09-22 17:23:47 +0000

    recherche dashboard

--- src/Components/Dashbord/SaisirDepense.jsx
+++ src/Components/Dashbord/SaisirDepense.jsx
@@ -53,48 +53,59 @@ function SaisirDepense() {
         </div>
       </div>
       <div className="corpsdashbord">
-
         {/* ------formulaire------------- */}
-       
+
         <form className="row g-3">
-            <div className="col-md-6">
-                <label for="inputEmail4" class="form-label">Email</label>
-                <input type="email" className="form-control" id="inputEmail4"/>
-            </div>
-            <div class="col-md-6">
-                <label for="inputPassword4" class="form-label">Password</label>
-                <input type="password" className="form-control" id="inputPassword4"/>
-            </div>
-            <div class="col-12">
-                <label for="inputAddress" class="form-label">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-            </div>
-            <div class="col-12">
-                <label for="inputAddress2" class="form-label">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-            </div>
-            <div class="col-md-6">
-                <label for="inputCity" class="form-label">City</label>
-                <input type="text" class="form-control" id="inputCity"/>
-            </div>
-            <div class="col-md-4">
-                <label for="inputState" class="form-label">State</label>
-                <select id="inputState" class="form-select">
-                <option selected>Choose...</option>
-                <option>...</option>
-                </select>
-            </div>
-            
-            <div class="col-12">
-                
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </div>
+          <div className="col-md-6">
+            <label for="inputEmail4" class="form-label">
+              Montant{" "}
+            </label>
+            <input type="email" className="form-control" id="inputEmail4" />
+          </div>
+          <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">
+              Date
+            </label>
+            <input type="date" className="form-control" id="inputPassword4" />
+          </div>
+          <div class="col-12">
+            <label for="inputAddress" class="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+            />
+          </div>
+          <div class="col-12">
+            <label for="inputAddress2" class="form-label">
+              Address 2
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="inputAddress2"
+              placeholder="Apartment, studio, or floor"
+            />
+          </div>
+          <div class="col-md-6">
+            <label for="inputCity" class="form-label">
+              City
+            </label>
+            <input type="text" class="form-control" id="inputCity" />
+          </div>
+
+          <div class="col-12"></div>
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary">
+              Sign in
+            </button>
+          </div>
         </form>
       </div>
-    </div>  
-
+    </div>
   );
 }
 export default SaisirDepense;
